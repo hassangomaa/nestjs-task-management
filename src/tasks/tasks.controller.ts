32638fd0aeaec2e8e9df9be4ac91d5bfd7c3b25f
hassangomaa <hassangomaa974@gmail.com>
2023-08-29 21:35:30 +0300

    protect whole tasks with jwt

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 // import { Task } from './task.model';
 import { CreateTaskDto } from './dot/create-task.dto';
@@ -6,8 +6,10 @@ import { create } from 'domain';
 import { GetTasksFilterDto } from './dot/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
 
     
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,11 +4,12 @@ import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Task]),  //array of repositories that we want to inject
-    
+    AuthModule,
   ],
   controllers: [
     TasksController
